321ce767ac9e636978addbd2bfaf091df00aaa9f
Brian Espino <wpd7p7v9wr@privaterelay.appleid.com>
2025-10-31 22:43:36 -0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,3 @@
-export default async function handler(req, res) {
-  return res.status(200).json({ message: "Chema Test Function Online" });
+export default async function handler(request, response) {
+  return response.status(200).json({ message: "Chema Test Function Online" });
 }
